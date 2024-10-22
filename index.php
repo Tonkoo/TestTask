54050d2250868e30e75d6c61567b5e8cd38cc64d
Tonkoo <andrey54374@gmail.com>
2024-10-22 11:29:20 +0300

    Добавил столбец 'Действия' в таблицу для ссылок на изменение записи и увольнение сотрудника. Доработал форму: теперь отображаются данные в полях 'Отдел' и 'Должность'. Реализовал функциональность добавления сотрудников. Для полей 'Серия/номер паспорта' и 'Контактная информация' добавил маску ввода.

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
+<?php
+    $conn = new mysqli("localhost", "root", "", "testtask");
+
+    if($conn->connect_error)
+    {
+        die("Ошибка: " . $conn->connect_error);
+    }
+    $id = 0;
+    $Lastname = "";
+    $Firstname = "";
+    $Middlename = "";
+    $SeriyaNomerPasport = "";
+    $ContactInformation = "";
+    $Adres = "";
+    $Otdel = "";
+    $Doljnost = "";
+    $SalaryAmount = "";
+    $DateEmployment = "";
+    $update = false;
+
+    if(isset($_GET['edit']))
+    {
+        $id = $_GET['edit'];
+        $update = true;
+        $sql = "SELECT id, Lastname, Firstname, Middlename, SeriyaNomerPasport, ContactInformation, Adres, idOtdela, idDoljnosti, SalaryAmount, DateEmployment, Dismissed FROM sotrudniki where id = ". $id;
+        $record = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($record);
+        $Lastname = $row['Lastname'];
+        $Firstname = $row['Firstname'];
+        $Middlename = $row['Middlename'];
+        $SeriyaNomerPasport = $row['SeriyaNomerPasport'];
+        $ContactInformation = $row['ContactInformation'];
+        $Adres = $row['Adres'];
+        $Otdel = $row['idOtdela'];
+        $Doljnost = $row['idDoljnosti'];
+        $SalaryAmount = $row['SalaryAmount'];
+    }
+
+    if(isset($_POST['add']))
+    {
+        $Lastname = $_POST['TbFamiliya'];
+        $Firstname = $_POST['TbImya'];
+        $Middlename = $_POST['TbOtchestvo'];
+        $SeriyaNomerPasport = $_POST['TbPasport'];
+        $ContactInformation = $_POST['TbTelephone'];
+        $Adres = $_POST['TbAdres'];
+        $Otdel = $_POST['CbAddOtdel'];
+        $Doljnost = $_POST['CbAddDlojnost'];
+        $SalaryAmount = $_POST['TbSalaryAmount'];
+        $sqlInsert = "insert into sotrudniki (Lastname, Firstname, Middlename,SeriyaNomerPasport, ContactInformation, Adres, idOtdela, idDoljnosti,SalaryAmount, DateEmployment, Dismissed)
+        VALUES('$Lastname', '$Firstname', '$Middlename', '$SeriyaNomerPasport', '$ContactInformation', '$Adres', '$Otdel', '$Doljnost', '$SalaryAmount', CURDATE(), 0)";
+        mysqli_query($conn, $sqlInsert);
+        header('location: index.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,23 +68,17 @@
     </header>
     <main>
         <?php
-            $conn = new mysqli("localhost", "root", "", "testtask");
-
-            if($conn->connect_error)
-            {
-                die("Ошибка: " . $conn->connect_error);
-            }
-            $Otdel = isset($_GET['CbOtdel']) ? $_GET['CbOtdel'] : '';
-            $Doljnost = isset($_GET['CbDlojnost']) ? $_GET['CbDlojnost'] : '';
+            $OtdelFilter = isset($_GET['CbOtdel']) ? $_GET['CbOtdel'] : '';
+            $DoljnostFilter = isset($_GET['CbDlojnost']) ? $_GET['CbDlojnost'] : '';
             $Poisk = isset($_GET['TbPoisk']) ? $_GET['TbPoisk'] : '';
-            $sql = "SELECT sotrudniki.id, Lastname, Firstname, Middlename, SeriyaNomerPasport, ContactInformation, Address, otdel.Name as OtdelName, doljnost.Name as DoljnostName, SalaryAmount, DateEmployment, Dismissed FROM sotrudniki join otdel on sotrudniki.idOtdela = otdel.id JOIN doljnost on sotrudniki.idDoljnosti = doljnost.id where 1=1";
-            if($Otdel)
+            $sql = "SELECT sotrudniki.id, Lastname, Firstname, Middlename, SeriyaNomerPasport, ContactInformation, Adres, otdel.Name as OtdelName, doljnost.Name as DoljnostName, SalaryAmount, DateEmployment, Dismissed FROM sotrudniki join otdel on sotrudniki.idOtdela = otdel.id JOIN doljnost on sotrudniki.idDoljnosti = doljnost.id where 1=1";
+            if($OtdelFilter)
             {
-                $sql .= " and idOtdela = ". $Otdel;
+                $sql .= " and idOtdela = ". $OtdelFilter;
             }
-            if($Doljnost)
+            if($DoljnostFilter)
             {
-                $sql .= " and idDoljnosti = ". $Doljnost;
+                $sql .= " and idDoljnosti = ". $DoljnostFilter;
             }
             if($Poisk)
             {
@@ -37,7 +87,7 @@
             if($result = $conn -> query($sql))
             {
                 $rowsCount = $result -> num_rows;
-                echo "<table><tr><th>Код</th><th>Фамилия</th><th>Имя</th><th>Отчество</th><th>Серия/Номер паспорт</th><th>Контактная информация</th><th>Адрес проживания</th><th>Отдел</th><th>Должность</th><th>Размер зарплаты</th><th>Дата принятия на работу</th><th>Статус</th></tr>";
+                echo "<table><tr><th>Код</th><th>Фамилия</th><th>Имя</th><th>Отчество</th><th>Серия/Номер паспорт</th><th>Контактная информация</th><th>Адрес проживания</th><th>Отдел</th><th>Должность</th><th>Размер зарплаты</th><th>Дата принятия на работу</th><th>Статус</th><th>Действия</th></tr>";
                 foreach($result as $row)
                 {
                     echo "<tr>";
@@ -47,12 +97,13 @@
                         echo "<td>".$row["Middlename"]."</td>";
                         echo "<td>".$row["SeriyaNomerPasport"]."</td>";
                         echo "<td>".$row["ContactInformation"]."</td>";
-                        echo "<td>".$row["Address"]."</td>";
+                        echo "<td>".$row["Adres"]."</td>";
                         echo "<td>".$row["OtdelName"]."</td>";
                         echo "<td>".$row["DoljnostName"]."</td>";
                         echo "<td>".$row["SalaryAmount"]."</td>";
                         echo "<td>".$row["DateEmployment"]."</td>";
                         echo "<td>".$row["Dismissed"]."</td>";
+                        echo "<td><a href='index.php?edit=".$row['id']."'>Изменить</a><br><a>Уволить</a></td>";
                     echo "</tr>";
                 }
                 echo "</table>";
@@ -108,17 +159,63 @@
             </form>
         </div>
         <div class = "BlockAddEdit">
-            <Form>
-                <p>Фамилия: <input type="text" name = "TbFamiliya"></p>
-                <p>Имя: <input type="text" name = "TbImya"></p>
-                <p>Отчество: <input type="text" name = "TbOtchestvo"></p>
-                <p>Серия/номер паспорт: <input type="text" name = "TbPasport"></p>
-                <p>Контактная информация: <input type="text" name = "TbTelephone"></p>
-                <p>Адрес: <input type="text" name = "TbAdress"></p>
-                <p>Отдел: <input type="text" name = "TbOtdel"></p>
-                <p>Должность: <input type="text" name = "TbDoljnost"></p>
-                <p>Размер зарплаты: <input type="text" name = "TbZp"></p>
-                <button type = "submit">Сохранить</button>
+            <h2><?php echo $update ? 'Изменить запись' : 'Добавить запись'; ?></h2>
+            <Form method = "POST">
+                <input type="hidden" name = "id" value = "<?php echo $id; ?>">
+                <p>Фамилия: <input type="text" name = "TbFamiliya" value = "<?php echo $Lastname; ?>"></p>
+                <p>Имя: <input type="text" name = "TbImya" value = "<?php echo $Firstname; ?>"></p>
+                <p>Отчество: <input type="text" name = "TbOtchestvo" value = "<?php echo $Middlename; ?>"></p>
+                <p>Серия/номер паспорт: <input type="text" name = "TbPasport" pattern = "\d{4} \d{6}" placeholder="0000 000000" value = "<?php echo $SeriyaNomerPasport; ?>"></p>
+                <p>Контактная информация: <input type="text" name = "TbTelephone" pattern = "\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}" placeholder="+7 (000) 000-00-00" value = "<?php echo $ContactInformation; ?>"></p>
+                <p>Адрес: <input type="text" name = "TbAdres" value = "<?php echo $Adres; ?>"></p>
+                <p>Отдел: <select name="CbAddOtdel">
+                        <?php
+                            $sql = "select id, Name from otdel";
+                            if($result = $conn ->query($sql))
+                            {
+                                $rowsCount = $result -> num_rows;
+                                echo "<option disabled selected>Выберите должность</option>";
+                                foreach($result as $row)
+                                {
+                                    if($row["id"] == $Otdel)
+                                        echo "<option value=".$row["id"]." selected>".$row["Name"]."</option>";
+                                    else
+                                        echo "<option value=".$row["id"].">".$row["Name"]."</option>";
+                                }
+                                $result ->free();
+                            }
+                            else
+                                echo "Ошибка: ". $conn->error;
+                        ?>
+                    </select>
+                </p>
+                <p>Должность: <select name="CbAddDlojnost">
+                        <?php
+                            $sql = "select id, Name from doljnost";
+                            if($result = $conn ->query($sql))
+                            {
+                                $rowsCount = $result -> num_rows;
+                                echo "<option disabled selected>Выберите должность</option>";
+                                foreach($result as $row)
+                                {
+                                    if($row["id"] == $Otdel)
+                                        echo "<option value=".$row["id"]." selected>".$row["Name"]."</option>";
+                                    else
+                                        echo "<option value=".$row["id"].">".$row["Name"]."</option>";
+                                }
+                                $result ->free();
+                            }
+                            else
+                                echo "Ошибка: ". $conn->error;
+                        ?>
+                    </select>
+                </p>
+                <p>Размер зарплаты: <input type="text" name = "TbSalaryAmount" value = "<?php echo $SalaryAmount; ?>"></p>
+                <?php if($update): ?>
+                    <button type="submit" name="save">Сохранить</button>
+                <?php else: ?>
+                    <button type="submit" name="add">Добавить</button>
+                    <?php endif; ?>
             </Form>
         </div>
     </main>
